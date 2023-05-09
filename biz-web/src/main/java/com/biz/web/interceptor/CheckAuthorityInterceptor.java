e0fc431b4a246b85aa2f92bbfd6c295a712f1a43
杜海航 <17686519394@163.com>
2023-05-09 18:46:16 +0800

    1、增加了检查接口权限拦截器；

---
+++ biz-web/src/main/java/com/biz/web/interceptor/CheckAuthorityInterceptor.java
@@ -0,0 +1,14 @@
+package com.biz.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * 检查用户权限
+ *
+ * @author francis
+ * @create: 2023-05-09 18:45
+ **/
+@Slf4j
+public class CheckAuthorityInterceptor implements HandlerInterceptor  {
+}
