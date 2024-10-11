649f29b2a3d6faea565b8c6ad94852cd226e6b3a
杜海航 <17686519394@163.com>
2024-10-11 10:10:51 +0800

    fix🐛: 修复了认证方式工厂下认证方式的注入
    修复了AuthenticationFactory认证方式工厂下使用的认证方式注入方式

--- biz-security/src/main/java/com/biz/security/authentication/AuthenticationFactory.java
+++ biz-security/src/main/java/com/biz/security/authentication/AuthenticationFactory.java
@@ -1,5 +1,7 @@
 package com.biz.security.authentication;
 
+import com.biz.security.authentication.type.AuthType;
+
 /**
  * 认证方式工厂类，提供认证方式的实例化
  *
@@ -9,14 +11,50 @@ package com.biz.security.authentication;
  **/
 public class AuthenticationFactory {
 
-    public static AuthenticationService getAuthenticationService(AuthMethodType methodType) {
+    /**
+     * 用户名密码认证方式
+     */
+    private final UsernamePasswordAuthenticationService usernamePasswordAuthenticationService;
+
+    /**
+     * OAuth2认证方式
+     */
+    private final OAuth2AuthenticationService oAuth2AuthenticationService;
+
+    /**
+     * Token认证方式
+     */
+    private final TokenAuthenticationService tokenAuthenticationService;
+
+
+    /**
+     * 构造函数，注入各个认证方式的实例
+     *
+     * @param usernamePasswordAuthenticationService 用户名密码认证方式实例
+     * @param oAuth2AuthenticationService           OAuth2认证方式实例
+     * @param tokenAuthenticationService            Token认证方式实例
+     */
+    public AuthenticationFactory(UsernamePasswordAuthenticationService usernamePasswordAuthenticationService, OAuth2AuthenticationService oAuth2AuthenticationService, TokenAuthenticationService tokenAuthenticationService) {
+        this.usernamePasswordAuthenticationService = usernamePasswordAuthenticationService;
+        this.oAuth2AuthenticationService = oAuth2AuthenticationService;
+        this.tokenAuthenticationService = tokenAuthenticationService;
+    }
+
+
+    /**
+     * 根据认证方式类型获取对应的认证服务实例
+     *
+     * @param methodType 认证方式类型
+     * @return 认证服务实例
+     */
+    public AuthenticationService getAuthenticationService(AuthType methodType) {
         switch (methodType) {
             case USERNAME_PASSWORD:
-                return new UsernamePasswordAuthenticationService();
+                return usernamePasswordAuthenticationService;
             case OAUTH2:
-                return new OAuth2AuthenticationService();
+                return oAuth2AuthenticationService;
             case TOKEN:
-                return new TokenAuthenticationService();
+                return tokenAuthenticationService;
             default:
                 throw new IllegalArgumentException("不支持的认证方式");
         }
